0401493c15f009a8b055206e2b034557e929ff3f
GUchechi <godswilluchendu7@gmail.com>
2022-11-13 19:28:55 +0100

    blossom parlour

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,51 @@ const Home = () => {
   return (
     <Section>
         <Navbar />
+        <div className="home">
+          <div className="content">
+            <div className="title">
+              <h1>Blossoms Parlour</h1>
+            </div>
+            <div className="subTitle">
+              <p>At the beauty Parlour, we believe in beauty with a conscience
+                We have created a beautiful salon that offers the highest quality 
+                hair services.
+              </p>
+            </div>
+            <img src={play} alt="Play Button" />
+          </div>
+        </div>
+        <div className="info">
+          <div className="grid">
+            <div className="col">
+              <strong>Parlour</strong>
+              <p>Blossoms Academy</p>
+            </div>
+            <div className="col">
+                <strong>Email</strong>
+                <p>blossoms@gmail.com</p>
+            </div> 
+            <div className="col">
+                <strong>Mobile</strong>
+                <p>+2348142827949</p>
+            </div> 
+            <div className="col">
+                <strong>Address</strong>
+                <p>322, Long Fly,</p>
+                <p>Abuja Nigeria</p>
+            </div> 
+            <div className="col">
+                <strong>Services</strong>
+                <p>Sparking</p>
+                <p>SPA Cream</p>
+            </div> 
+            <div className="col">
+                <strong>Working Hours</strong>
+                <p>Monday to Friday</p>
+                <p>08:00 to 09:00</p>
+            </div>   
+          </div>
+        </div>
     </Section>
   )
 }
@@ -17,6 +62,46 @@ const Section = styled.section`
     min-height: 100vh;
     background-size: cover;
     position: relative;
+    .home {
+      
+      height: 100%;
+      .content{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        width: 60%;
+        color: #fff;
+        gap: 1.2rem;
+        margin-top: 8rem;
+        padding-left: 18rem;
+        .title {
+          h1{
+            font-size: 5rem;
+            line-height: 5.3rem;
+          }
+        }
+        .subTitle {
+          p{
+            width: 70%;
+            margin-bottom: 2rem;
+          }
+        }
+      }
+    }
+    .info {
+      position: absolute;
+      bottom: -8rem;
+      right: 0;
+      background:var(--secondary-color);
+      padding: 4rem;
+      .grid{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr) ;
+        gap: 4rem;
+        color: #fff;
+      }
+    }
+
 `
 
 export default Home
