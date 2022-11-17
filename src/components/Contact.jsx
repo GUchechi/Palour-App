9113c4f82f9f1e47682fbe90f38325d65e67dbfb
GUchechi <godswilluchendu7@gmail.com>
2022-11-17 02:13:17 +0100

    blossom parlour

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -139,6 +139,34 @@ const Section = styled.section`
       }
     }
   }
+  @media screen and (min-width: 280px) and (max-width: 1080px) { 
+  .contact {
+    margin: 1rem;
+    padding: 0 1rem;
+    &_title {
+      margin: 0;
+      text-align: center;
+      p {
+        font-size: 0.8rem;
+      }
+      h2 {
+        font-size: 1.3rem;
+      }
+    }
+    &_data {
+      grid-template-columns: 1fr;
+      margin: 0;
+      p {
+        text-align: left;
+      }
+      &_form {
+        button {
+          height: 6rem;
+        }
+      }
+    }
+  }
+}
 `
 
 export default Contact
