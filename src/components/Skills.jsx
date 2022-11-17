cb0a15e0595a4830210d0c3ee23337c201c542ed
GUchechi <godswilluchendu7@gmail.com>
2022-11-17 09:12:59 +0100

    blossom parlour

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -147,7 +147,7 @@ const Section = styled.section`
   @media screen and (min-width: 280px) and (max-width: 1080px) { 
     overflow-x: hidden;
     padding: 2rem 0;
-    height: 120vh;
+    height: 100vh;
     .background {
       display: none;
     }
